db86e809588e0266edebcffb1608e91bb5c821d1
sualval <101261165+sualval@users.noreply.github.com>
2022-10-02 17:44:32 +0400

    add CreditCalculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    static double percent = 15;
+    static double creditAmount = 100_000;
+    static int month = 12;
+    int dayInYear = 365;
+    int dayInMonth = 31;
+    double monthlyAmount = creditAmount / month;
+    Scanner scanner;
+
     public static void main(String[] args) {
-
+        Main main = new Main();
+        System.out.println("Enter credit amount:");
+        creditAmount = main.ParametersForCalculation();
+        System.out.println("Enter number of months:");
+        month = main.ParametersForCalculation();
+        System.out.println(Arrays.toString(main.resultCreditCalculator(creditAmount, month)));
     }
 
-    static double overpaymentCalculation(double creditAmount, int month) {
-        return 0.0;
+    public Integer ParametersForCalculation() {
+        while (true) {
+            try {
+                scanner = new Scanner(System.in);
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException exception) {
+                System.out.println("NumberFormatException");
+            }
+        }
     }
 
-    static double calculatingTotalRefund(double creditAmount) {
-        return 0.0;
-    }
-
-    static double monthlyPaymentCalculation() {
-        return 0.0;
+    public double[] resultCreditCalculator(double creditAmount, int month) {
+        double overpaymentCalculation = 0;
+        double calculatingTotalRefund;
+        double monthlyPaymentCalculation;
+        double outstandingBalance = creditAmount;
+        for (int i = 0; i < month; i++) {
+            overpaymentCalculation += ((percent / 100 / dayInYear * dayInMonth) * outstandingBalance);
+            outstandingBalance -= monthlyAmount;
+        }
+        calculatingTotalRefund = overpaymentCalculation + creditAmount;
+        monthlyPaymentCalculation = calculatingTotalRefund / month;
+        return new double[]{overpaymentCalculation, calculatingTotalRefund, monthlyPaymentCalculation};
     }
 }
--- src/test/java/CreditCalculatorTest.java
+++ src/test/java/CreditCalculatorTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -10,9 +13,12 @@ public class CreditCalculatorTest {
     double overpaymentCalculation = 0;
     double calculatingTotalRefund;
     double monthlyPaymentCalculation;
+    double[] expected;
+     Main main = new Main();
 
     @BeforeEach
     void createNewClass() {
+
         double outstandingBalance = creditAmount;
         int dayInMonth = 31;
         int dayInYear = 365;
@@ -24,27 +30,25 @@ public class CreditCalculatorTest {
         }
         calculatingTotalRefund = overpaymentCalculation + creditAmount;
         monthlyPaymentCalculation = calculatingTotalRefund / month;
-    }
-
-    @Test
-    void TestMonthlyPaymentCalculation() {
-        double extend = monthlyPaymentCalculation;
-
-        assertEquals(extend, Main.monthlyPaymentCalculation());
+        expected = new double[]{overpaymentCalculation, calculatingTotalRefund, monthlyPaymentCalculation};
 
     }
 
     @Test
-    void TestCalculatingTotalRefund() {
-        double extend = calculatingTotalRefund;
-        assertEquals(extend, Main.calculatingTotalRefund(creditAmount));
+    void TestLengthArray() {
+
+        assertEquals(3, main.resultCreditCalculator(creditAmount, month).length);
     }
 
     @Test
-    void TestOverpaymentCalculation() {
-
-        double expected = overpaymentCalculation;
-        assertEquals(expected, Main.overpaymentCalculation(creditAmount, month));
+    void TestResultCreditCalculator() {
+        assertArrayEquals(expected, main.resultCreditCalculator(creditAmount, month), 1e-2);
     }
 
+    @Test
+    void TestParametersForCalculation() {
+        System.setIn(new ByteArrayInputStream("200000".getBytes()));
+        assertEquals(200000, main.ParametersForCalculation());
+
+    }
 }
